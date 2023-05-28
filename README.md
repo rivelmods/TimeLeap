2d70ecc1dccf6dd1c62719feaf6f2eb60e8335fc
rivelmods <abstractycoder@gmail.com>
2023-05-28 13:12:36 +0530

    Done

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# TimeLeap
-A lightweight timelapse recorder written in python.
+# Timelapse
+A simple command line timelapse recorder made for my personal use.
+
+> :warning: **This program only works on macOS and perhaps linux.
+
+### Dependencies
+- [pyautogui](https://pypi.org/project/PyAutoGUI/)
+- [ffmpeg](https://ffmpeg.org/)
+- [python3](https://www.python.org/)
+
+you can use [brew](https://brew.sh/) to install ffmpeg, which is a video encoder used in the program, using this command or from the official website.
+
+```
+brew install ffmpeg
+```
+pyautogui is a python library used for taking screenshots, which you can download using [pip](https://pypi.org/project/pip/) which you already know is a package manger for python.
+
+```
+pip3 install pyautogui
+```
+
+If you encouter some kind of error with pyautogui, you can try install [Pillow](https://pypi.org/project/Pillow/) with pip, which will fix it.
+
+```
+pip3 install Pillow
+```
+### How to use
+
+You can pass in two arguments the time you want to record and the fps of the finished project. Once you enter the command it will record a timelapse for the given amount of time. It will ask you if you want to render the video, in case you want to use an external program to do it. After rendering, you will find the video in the output folder.
+And it will also ask you if you want to clear the screenshots folder if you are no longer going to use the images. The command is give below. Replace record_time and fps with your desired values.
+
+```
+python3 timelapse.py -r <record_time> -f <fps>
+```
+
---
+++ timelapse.py
@@ -0,0 +1,90 @@
+print("Initializing the program...")
+
+import os
+import time
+import subprocess
+import getopt
+import sys
+import pyautogui
+
+def clear_screenshots():
+    print()
+    clear = input("Do you wish to clear the screenshots folder? (y/n): ")
+
+    if clear.lower() == 'y':
+        clear_cmd = "rm screenshots/*"
+        subprocess.run(clear_cmd, shell=True)
+    else:
+        sys.exit(2)
+
+
+def render_video(fps):
+    render_choice = input("Do you want to render the video? (y/n): ")
+
+    if render_choice.lower() == 'y':
+        video_name = input("What do you want to name the output video?: ")
+
+        render_cmd = f"cd screenshots && ffmpeg -framerate {fps} -pattern_type glob -i '*.png' \
+      -c:v libx264 -pix_fmt yuv420p out.mp4"
+
+        subprocess.run(render_cmd, shell=True)
+        os.rename("screenshots/out.mp4", "output/" + video_name +".mp4")
+
+        clear_screenshots()
+        print('Thank you for using the program.')
+
+    else:
+        print()
+        print('Thank you for using the program.')
+
+
+def record(record_time):
+    count = 1
+    images = []
+    recording = True
+
+    while recording:
+        image = pyautogui.screenshot()
+        image.save('screenshots/image_' + str(count) + '.png')
+        count += 1
+        images.append(image)
+        time.sleep(1)
+
+        if len(images) == record_time:
+            print('Recording is done')
+            break
+        else:
+            print('Recording', end="\r", flush=True)
+
+def get_args(argv, record_time, fps):
+    try:
+        opts, args = getopt.getopt(argv, "r:f:", ["record_time=", "fps="])
+    except getopt.GetoptError:
+        print('ERROR: usage - timelapse.py -r <record_time> -f <fps>')
+        sys.exit(2)
+
+    if not opts:
+        print('ERROR: usage - timelapse.py -r <record_time> -f <fps>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-r", "--record_time"):
+            record_time = int(arg)
+        elif opt in ("-f", "--fps"):
+            fps = arg
+
+    return record_time, fps
+
+
+def main(argv):
+    record_time = ''
+    fps = ''
+
+    record_time, fps = get_args(argv, record_time, fps)
+    print(input)
+
+    record(record_time)
+    render_video(fps)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
